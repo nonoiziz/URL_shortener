2085c16f080c42ab87b1411273b608f6c775c207
Matthew <minhsiutsai@gmail.com>
2023-07-20 12:52:25 +0800

    build: add expressHandlebars

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express')
-const app = express()
+const app = express();
 const port = 3000
 
+
+import express from 'express';
+import { engine } from 'express-handlebars';
+
+
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
 app.get('/', (req, res) => {
-  res.send('express app for movies')
-})
+    res.render('home');
+});
+
 
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`)
